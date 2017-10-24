4caac3e543516ec494e4ffe3b3d9eab6072ae6d2
WladimirTavares <wladimirufc@gmail.com>
2017-10-24 14:38:41 -0300

    BT_ORD_BY_SL modificado

--- BT_ORD_BY_SL.cpp
+++ BT_ORD_BY_SL.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int teto = 10000;
+int teto;
 
 
 
@@ -122,16 +122,11 @@ void bt_SL_Aux(Graph G, vector <int> & cor, int k,vector<int> ordem){
         if (teto > maior_cor){
             teto = maior_cor;
         }
-        imprime_coloracao(cor);
-        /*cout << endl;
-        cout << "Melhor cor Atual: "<< teto << endl;
-        cout << endl << endl;*/
         
-
-        
+        //imprime_coloracao(cor);
+        //cout << endl;
+        cout << "Encontrando coloracao: "<< teto+1 << endl;
     }else{
-
-
         int v = -1;
 
         for(int i = 0; i < ordem.size(); i++){
@@ -202,19 +197,17 @@ void bt_SL(Graph G){
 
 
     vector <int> cor;
+    
     cor.assign(G.n, -1);
 
 
     vector<int> ordem = SL(G);
+    
     int teto = coloring(ordem,G.n,G);
-
-    //cout << "teto : " << teto << endl;
-
+    
+    cout << "numero de cores iniciais: " << (teto+1) << endl;
 
     bt_SL_Aux(G,cor,0,ordem);
-
-
-
 
 
 }
@@ -298,45 +291,6 @@ int readFile(FILE * graphFile, Graph & G) {
     return 0;
 }
 
-char filename[121][1024] = {"./graph_color/le450_25d.col","./graph_color/mug100_25.col",
-    "./graph_color/4-Insertions_4.col","./graph_color/2-Insertions_4.col","./graph_color/myciel7.col",
-    "./graph_color/queen10_10.col","./graph_color/3-FullIns_4.col","./graph_color/4-FullIns_4.col",
-    "./graph_color/le450_5c.col","./graph_color/le450_25c.col","./graph_color/le450_5d.col",
-    "./graph_color/5-FullIns_4.col","./graph_color/queen16_16.col","./graph_color/mug88_1.col",
-    "./graph_color/miles250.col","./graph_color/2-FullIns_4.col","./graph_color/queen12_12.col",
-    "./graph_color/le450_25a.col","./graph_color/myciel3.col","./graph_color/queen8_8.col",
-    "./graph_color/DSJC250.5.col","./graph_color/qg.order40.col","./graph_color/mulsol.i.4.col",
-    "./graph_color/le450_15a.col","./graph_color/queen5_5.col","./graph_color/inithx.i.1.col",
-    "./graph_color/DSJC500.5.col","./graph_color/queen8_12.col","./graph_color/le450_5a.col",
-    "./graph_color/games120.col","./graph_color/DSJR500.1.col","./graph_color/DSJR500.5.col",
-    "./graph_color/DSJC500.1.col","./graph_color/school1_nsh.col","./graph_color/4-FullIns_5.col",
-    "./graph_color/3-Insertions_3.col","./graph_color/1-Insertions_5.col","./graph_color/myciel4.col",
-    "./graph_color/zeroin.i.3.col","./graph_color/wap08a.col","./graph_color/1-FullIns_5.col",
-    "./graph_color/fpsol2.i.1.col","./graph_color/3-FullIns_5.col","./graph_color/abb313GPIA.col",
-    "./graph_color/david.col","./graph_color/1-Insertions_6.col","./graph_color/mulsol.i.3.col",
-    "./graph_color/2-Insertions_5.col","./graph_color/miles1500.col","./graph_color/queen13_13.col",
-    "./graph_color/DSJC1000.9.col","./graph_color/wap02a.col","./graph_color/DSJC1000.5.clq",
-    "./graph_color/DSJC125.1.col","./graph_color/wap04a.col","./graph_color/2-Insertions_3.col",
-    "./graph_color/le450_15c.col","./graph_color/5-FullIns_3.col","./graph_color/queen6_6.col",
-    "./graph_color/fpsol2.i.2.col","./graph_color/mulsol.i.2.col","./graph_color/miles750.col",
-    "./graph_color/inithx.i.3.col","./graph_color/myciel5.col","./graph_color/wap05a.col",
-    "./graph_color/inithx.i.2.col","./graph_color/myciel6.col","./graph_color/le450_15b.col",
-    "./graph_color/queen9_9.col","./graph_color/miles1000.col","./graph_color/DSJC1000.1.col",
-    "./graph_color/ash958GPIA.col","./graph_color/1-Insertions_4.col","./graph_color/4-Insertions_3.col",
-    "./graph_color/queen11_11.col","./graph_color/3-Insertions_5.col","./graph_color/2-FullIns_3.col",
-    "./graph_color/huck.col","./graph_color/zeroin.i.2.col","./graph_color/anna.col","./graph_color/DSJC250.1.col",
-    "./graph_color/DSJC125.9.col","./graph_color/zeroin.i.1.col","./graph_color/mulsol.i.5.col",
-    "./graph_color/3-Insertions_4.col","./graph_color/DSJC125.5.col","./graph_color/will199GPIA.col",
-    "./graph_color/jean.col","./graph_color/wap07a.col","./graph_color/mug100_1.col","./graph_color/le450_15d.col",
-    "./graph_color/le450_5b.col","./graph_color/mug88_25.col","./graph_color/mulsol.i.1.col","./graph_color/wap06a.col",
-    "./graph_color/wap03a.col","./graph_color/queen15_15.col","./graph_color/latin_square_10.col",
-    "./graph_color/DSJC500.5.clq","./graph_color/wap01a.col","./graph_color/homer.col","./graph_color/school1.col",
-    "./graph_color/DSJR500.1c.col","./graph_color/ash608GPIA.col","./graph_color/le450_25b.col",
-    "./graph_color/DSJC1000.5.col","./graph_color/DSJC250.9.col","./graph_color/qg.order100.col",
-    "./graph_color/qg.order30.col","./graph_color/queen14_14.col","./graph_color/3-FullIns_3.col",
-    "./graph_color/fpsol2.i.3.col","./graph_color/1-FullIns_4.col","./graph_color/1-FullIns_3.col",
-    "./graph_color/ash331GPIA.col","./graph_color/2-FullIns_5.col","./graph_color/4-FullIns_3.col",
-    "./graph_color/miles500.col","./graph_color/DSJC500.9.col","./graph_color/queen7_7.col","./graph_color/qg.order60.col"};
 
 
 
@@ -345,64 +299,30 @@ char filename[121][1024] = {"./graph_color/le450_25d.col","./graph_color/mug100_
 
 int main(){
 
-    // Graph G(5);
-    // G.edge(0,3);
-    // G.edge(1,2);
-    // G.edge(1,3);
-    // G.edge(1,4);
-    // G.edge(2,3);
-    // G.edge(3,4);
-    
+	
+	for(int i = 1; i <= 5; i++)
+		for(int j = 1; j <= 9; j++)
+			for(int k=1; k<= 10; k++){
+				char filename[1024];
+				sprintf(filename,"./randomgraph/%d.0.%d.%d", 10*i, 10*j, k);
+			    
+				FILE *infile;    
+				printf("%s\n", filename);
+				if((infile=fopen(filename,"r"))==NULL)
+				{
+					printf("Error in graph file\n");
+					exit(0);
+				}
 
-
-    
-    // ordem.push_back(1);
-    // ordem.push_back(2);
-    // ordem.push_back(3);
-    // ordem.push_back(4);
-    // ordem.push_back(0);
-
-
-    // cor.assign(5, -1);
-
-
-    // cout << "SEM PODA" << endl;
-
-    // int n, m;
-
-    //     cin >> n >> m;
-
-    //     Graph G(n);
-
-    //     for(int i = 1; i <= m; i++) {
-    //         int a, b;
-    //         cin >> a >> b;
-    //         G.edge(a, b);
-    //     }
-
-
-    // bt_SL(G);
-
-    for(int i = 0; i < 121; i++){
-            FILE *infile;    
-            printf("%s\n", filename[i]);
-            if((infile=fopen(filename[i],"r"))==NULL)
-            {
-                printf("Error in graph file\n");
-                exit(0);
-            }
-
-            Graph G;    
-            /* read graph */
-            if( readFile(infile, G) == 0){
-                vector <int> order;
-                order = SL(G);
-                //int chi;
-                //chi = tet;//coloring(order, G.n, G.M);
-                //cout << (chi+1) << endl;
-            }else{
-                printf("Error in readFile\n");
-            }
+				Graph G;    
+				/* read graph */
+				if( readFile(infile, G) == 0){
+					teto = G.n;
+					bt_SL(G);
+					cout << (teto+1) << endl;
+				}else{
+					printf("Error in readFile\n");
+				}
             
         }     
 
